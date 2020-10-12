800067cd161c6a8617bb97ba08148decd3116abe
Kelsi Proulx <kelsiproulx1919@gmail.com>
2020-10-12 15:31:27 +0000

    add planning file

---
+++ math_game_planning.rb
@@ -0,0 +1,7 @@
+=begin
+Class brainstorm: math, players, turns, question
+math class needs to store the logic for picking two numbers between 1-20
+players class should track the lives [score] remaining (out of three), life is lost if they answer a question incorrectly. Score should be displayed for both players after each turn
+turns class should track the current turn (who is the current_player) <= maybe move this into players
+question tracks what current and previous questions are (should only be able to use a question once)
+=end
